f5f0b9acd8fab42309ca9a3aae362a2e2ec27f9e
Rupert Adams <RupertNCAdams@aol.com>
2018-11-18 20:18:39 +0000

    index js and tests added, awaiting api connection

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 fetch('https://content.guardianapis.com/search?api-key=187b19a7-731e-4de8-bac8-7eebba1306c2');
   .then(response => response.json())
   .
+
+window.addEventListener("load", function() {
+
+  news = new News();
+
+  document.getElementById("app").innerHTML = IndexPage().render(news.listIndex());
+}, false)
--- spec/news_spec.js
+++ spec/news_spec.js
@@ -1,12 +1,18 @@
 let helloWorld;
 
 console.log("--TEST INITIATION--")
+
 console.log('Test should output Hello World');
 news = new News();
 expect(news.test()).toEqual("Hello World");
 
 console.log("--INDEX--");
-console.log('Index should render correctly');
+
+console.log('List Index should render correctly');
 list = new ListIndex();
 story = news.testStory();
 expect(list.render(story)).toHaveContent('<div class="container">');
+
+console.log('Index should render correctly')
+index = new IndexPage;
+expect(index.render(story)).toHaveContent("This is a test story");
